19c276a70f706dc1280454f93e31ad24420d295e
srinathLN7 <lakshminarayanan.nandakumar@cgi.com>
2022-08-04 14:52:28 +0200

    init feature branch

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,11 @@
 module Main where
 
-import qualified Nim  
+import Nim  
 
-type Board = [Int]
 
-initial :: Board
-initial = [5,4,3,2,1]
+-- type Board = [Int]
+-- initial :: Board
+-- initial = [5,4,3,2,1]
 
 main :: IO ()
-main = Nim.play initial 1 
+main =  startNim 
--- src/Nim.hs
+++ src/Nim.hs
@@ -1,36 +1,32 @@
-module Nim (play) where
-
--- import Data.Char 
-import Data.Maybe (fromJust)
+module Nim (startNim) where
 import Text.Read
 
 -- GAME UTILITIES
-
-type Row = Int
-type Number = Int 
+type Row = Int 
 type Board = [Row]
 
-initial :: Board
-initial = [5,4,3,2,1]
+initialB :: Board
+initialB = [5,4,3,2,1]
 
-next :: Int -> Maybe Int
-next 1 = Just 2
-next 2 = Just 1
-next _ = Nothing 
+data GameState = GameState {_nRows :: Row, _board :: Board, _currentPlayer :: Player} deriving Show
+data Player =  Player_1 | Player_2 deriving Show
 
-
+next :: Player -> Player
+next Player_1 = Player_2
+next Player_2 = Player_1
+ 
 finished:: Board -> Bool
 finished = all (==0)
 
-valid :: Board -> Row -> Number -> Bool
+valid :: Board -> Row -> Int -> Bool
 valid board row num =  board !! (row -1) >= num  
 
-move :: Board -> Row -> Number -> Board 
+move :: Board -> Row -> Int -> Board 
 move board row num = [if row == r then n - num else n |(r,n) <- zip [1..] board]
 
 
 -- SCREEN UTILITIES
-putRow :: Row -> Number -> IO ()
+putRow :: Row -> Int -> IO ()
 putRow row num = do putChar '\n'
                     putStr (show row ++ ": ")
                     print ( concat $ replicate num "*")
@@ -40,12 +36,6 @@ putRow row num = do putChar '\n'
 putBoard :: Board -> IO ()
 putBoard board = sequence_ [ putRow r b | (r,b) <- zip [1..] board]
 
-
-rowPrompt :: String
-rowPrompt = "Enter row number: "
-
-numPrompt :: String
-numPrompt = "Enter number of stars to remove: "
 
 newline :: IO ()
 newline = putChar '\n'
@@ -59,29 +49,44 @@ getNumber prompt       = do putStrLn prompt
                                            newline     
                                            getNumber prompt 
                              Just y  -> case prompt of  
-                                            "Enter row number: " -> if y `elem` initial then
+                                            "Enter row number: " -> if y `elem` initialB then
                                                          return y
                                                          else do putStrLn "Invalid row number. Please try again."
                                                                  newline   
-                                                                 getNumber rowPrompt 
+                                                                 getNumber "Enter row number: " 
                                             _ -> return y   
                                                            
 
 --  MAIN GAME FUNCTION
-play :: Board -> Int -> IO ()
+play :: Board -> Player -> IO ()
 play board player = do putBoard board
                        newline 
                        if finished board then
-                           putStrLn ("Player " ++ show (fromJust $ next player) ++ " wins!!!")
+                           putStrLn (show (next player) ++ " wins!!!")
                        else 
-                           do print ("Playing now --> Player " ++ show player)
+                           do print (show player ++ " ----------------> Turn")
                               newline
-                              row <- getNumber rowPrompt 
+                              row <- getNumber "Enter row number: " 
                               newline   
-                              num <- getNumber numPrompt
+                              num <- getNumber "Enter number of stars to remove: "
                               newline  
                               if valid board row num then
-                                play (move board row num) (fromJust $ next player)
+                                play (move board row num) (next player)
                               else
                                   do putStrLn "ERROR: INVALID MOVE!!! Please try again"
                                      play board player                 
+
+
+startNim :: IO ()
+startNim = do newline  
+              putStrLn "*************************************************************************** Welcome to the game of Nim ***************************************************************************"
+              play initialB Player_1 
+              --  newline
+              --  putStrLn "Enter the total number of rows in the board: "
+              --  x <- getLine
+              --  case (readMaybe x :: Maybe Int) of
+              --     Nothing -> do putStrLn "Please enter a valid number and try again."
+              --                   newline
+              --                   startNim
+              --     Just nRows  -> do putStrLn "Constructing the Nim game board"
+              --                       let initial = [nRows .. 1] in play initial Player_1  
