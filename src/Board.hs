58ac49dd332f4a2240091cb6d20b852cd9030ca8
srinathLN7 <lakshminarayanan.nandakumar@cgi.com>
2022-08-05 15:59:52 +0200

    add board rendering

--- src/Board.hs
+++ src/Board.hs
@@ -2,6 +2,7 @@ module Board where
 
 import Text.Read
 import System.Console.ANSI
+import Control.Monad
 
 
 -- GAME UTILITIES
@@ -14,28 +15,42 @@ next :: Player -> Player
 next PLAYER_1 = PLAYER_2
 next PLAYER_2 = PLAYER_1
 
+
+renderPoint :: (Int, Int) -> IO ()
+renderPoint (r, c) = setCursorPosition r c >> putChar '*'
+
 renderWelcome :: IO () 
 renderWelcome = do setSGR [SetColor Foreground Vivid Blue]
-                   Just (_, mcol) <- getTerminalSize     
-                   setCursorColumn (mcol `div` 4) >> putStrLn "*********************************** Welcome to the Variant NIM ***********************************" 
+                   Just (_, mcol) <- getTerminalSize
+                   Just (crow, _) <- getCursorPosition
+                   forM_ (zip (repeat crow) [0 .. mcol]) renderPoint                             
+                   setCursorPosition (crow + 1) (mcol `div` 3) >> putStrLn "Welcome to the Variant NIM game" 
+                   forM_ (zip (repeat (crow+2)) [0 .. mcol]) renderPoint
                    setSGR [Reset]
-                           
-renderErrMsg :: IO ()
-renderErrMsg = do setSGR [SetColor Foreground Vivid Red]
-                  putStrLn "ERROR: INVALID MOVE!!! Please try again"      
-                  setSGR [Reset]      
+
+
+renderErrMsg :: String -> IO ()
+renderErrMsg err = do setSGR [SetColor Foreground Vivid Red]
+                      putStrLn err      
+                      setSGR [Reset]      
+
 
 renderPlayerMsg :: Player -> IO ()
-renderPlayerMsg player = do setSGR [SetColor Foreground Vivid Yellow]    
+renderPlayerMsg player = do setSGR [SetColor Foreground Vivid Yellow]
+                            Just (_, mcol) <- getTerminalSize
+                            setCursorColumn (mcol `div` 3)    
                             print ( "PLAYING now  - " ++ show player)
                             setSGR[Reset]
 
 
 renderWinMsg :: Player -> IO ()
 renderWinMsg player = do setSGR [SetColor Foreground Vivid Green]
-                         putStrLn (show (next player) ++ " WINS!!!")
+                         Just (_, mcol) <- getTerminalSize
+                         setCursorColumn (mcol `div` 3)         
+                         putStrLn (show (next player) ++ " WINS !!!")
                          newline 
                          setSGR[Reset]
+
 
 putRow :: Row -> Int -> IO ()
 putRow row num = do setSGR [SetColor Foreground Vivid Blue]
@@ -47,9 +62,7 @@ putRow row num = do setSGR [SetColor Foreground Vivid Blue]
 
 -- sequence_ :: [IO a] -> IO ()
 putBoard :: Board -> IO ()
-putBoard board = do Just (_, mcol) <- getTerminalSize
-                    setCursorColumn (mcol `div` 2) 
-                    sequence_ [ putRow r b | (r,b) <- zip [1..] board]
+putBoard board = sequence_ [ putRow r b | (r,b) <- zip [1..] board]
 
 
 newline :: IO ()
@@ -59,14 +72,16 @@ getNumber :: Row -> String -> IO Int
 getNumber mRows prompt       = do putStrLn prompt
                                   x <- getLine                                           
                                   case (readMaybe x :: Maybe Int) of
-                                    Nothing -> do putStrLn "Invalid input. Only numbers accepted. Please try again"
+                                    Nothing -> do newline
+                                                  renderErrMsg "ERROR: Invalid number input !!! Please try again"
                                                   newline     
                                                   getNumber mRows prompt 
                                     Just y  -> case prompt of  
                                                 "Enter row number: " -> let initBoard = reverse[1..mRows] in 
                                                                         if y `elem` initBoard then
                                                                         return y        
-                                                                        else do putStrLn "Invalid row number. Please try again."
+                                                                        else do newline
+                                                                                renderErrMsg "ERROR: Invalid row number!!! Please try again."
                                                                                 newline   
                                                                                 getNumber mRows "Enter row number: " 
                                                 _ -> return y   
--- src/Nim.hs
+++ src/Nim.hs
@@ -18,7 +18,7 @@ play mRows board player = do putBoard board
                                    if valid board row num then
                                         play mRows (move board row num) (next player)
                                     else
-                                        do renderErrMsg
+                                        do renderErrMsg "ERROR: INVALID MOVE!!! Please try again"
                                            play mRows board player                 
 
 
@@ -31,18 +31,24 @@ valid board row num =  board !! (row -1) >= num
 move :: Board -> Row -> Int -> Board 
 move board row num = [if row == r then n - num else n |(r,n) <- zip [1..] board]
 
-
 startNim :: IO ()
 startNim = do newline  
               renderWelcome 
               newline
-              putStrLn "Enter the total number of rows in the board: "
-              x <- getLine
+              newline
+              putStrLn "Enter the total number of rows you want in the board: "
+              x <- getLine 
               case (readMaybe x :: Maybe Int) of
-                  Nothing -> do putStrLn "Please enter a valid number and try again."
+                  Nothing -> do renderErrMsg "ERROR: Invalid number input !!! Please try again."
                                 newline
                                 startNim
-                  Just nRows  -> do  newline  
-                                     putStrLn "Constructing the Nim game board"  
-                                     let iBoard = reverse [1..nRows] in 
-                                      play nRows iBoard PLAYER_1  
+                  Just nRows  -> if nRows >=5 then  
+                                    do  newline  
+                                        putStrLn "Constructing the Nim game board"  
+                                        let iBoard = reverse [1..nRows] in 
+                                         play nRows iBoard PLAYER_1  
+                                 else 
+                                    do  newline 
+                                        renderErrMsg "ERROR: Invalid Input!!! Number of rows must be greater than or equal to 5 (Standard Nim board). Please try again"
+                                        newline 
+                                        startNim
